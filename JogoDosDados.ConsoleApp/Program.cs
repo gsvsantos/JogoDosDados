8a759dfa98d6eb6ab9e1c8c056971ddc2db6d700
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-19 15:04:42 -0300

    feat: Implementada lógica de lançamento de dado.

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random numberGenerator = new Random();
+
+            do
+            {
+                Header();
+                Console.Write("Pressione [Enter] para lançar o dado...");
+                Console.ReadKey();
+
+                int result = numberGenerator.Next(1, 7);
+
+                Header();
+                Console.WriteLine("/==--==--==--==--==--==--==\\");
+                Console.WriteLine($"  O dado caiu no número: {result}");
+                Console.WriteLine("\\==--==--==--==--==--==--==/\n");
+
+                Console.Write("Deseja continuar? (S/N) ");
+                string optionContinue = Console.ReadLine()!.ToUpper();
+                if (optionContinue != "S" && optionContinue != "SIM" && optionContinue != "SI")
+                    break;
+
+            } while (true);
+        }
+        static void Header()
+        {
+            Console.Clear();
+            Console.WriteLine("/==--==--==--==--==--==--==--==--==--==\\");
+            Console.WriteLine("            Jogo dos Dados");
+            Console.WriteLine("\\==--==--==--==--==--==--==--==--==--==/\n");
         }
     }
 }
