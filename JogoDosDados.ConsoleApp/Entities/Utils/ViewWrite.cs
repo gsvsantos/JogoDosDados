5485ec85cc04188b4617c5098b20fdd37b2533f5
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-02 00:18:40 -0300

    refactor: Adicionado `MainMenuOptions()` e `Goodbye()`.

--- JogoDosDados.ConsoleApp/Entities/Utils/ViewWrite.cs
+++ JogoDosDados.ConsoleApp/Entities/Utils/ViewWrite.cs
@@ -1,5 +1,16 @@
 ﻿namespace JogoDosDados.ConsoleApp.Entities.Utils;
 
-internal class ViewWrite
+public class ViewWrite
 {
+    public static void MainMenuOptions()
+    {
+        ViewUtils.PaintWrite("1 >> Começar a Partida\n", ConsoleColor.White);
+        ViewUtils.PaintWrite("2 >> Sobre o Jogo\n", ConsoleColor.White);
+        ViewUtils.PaintWrite("S >> Fechar Jogo\n", ConsoleColor.White);
+        ViewUtils.PaintWrite("\nOpção: ", ConsoleColor.White);
+    }
+    public static void Goodbye()
+    {
+        ViewUtils.PaintWrite("Adeus (T_T)/\n", ConsoleColor.Red);
+    }
 }
